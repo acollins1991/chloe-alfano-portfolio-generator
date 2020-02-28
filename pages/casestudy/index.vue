947846146fcca15bf9f7405fbc2058f1c91e3205
Anthony Collins <a.r.c_@hotmail.co.uk>
2020-02-28 10:27:57 +0000

    case studies index page to list case studies

---
+++ pages/casestudy/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="(caseStudy, index) in caseStudies" :key="index">
+      <a :href="$route.fullPath + '/' + caseStudy.slug">{{ caseStudy.slug }}</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'CaseStudiesHome',
+  data () {
+    return {
+      caseStudies: []
+    }
+  },
+  computed: {
+    ...mapState(['clientData'])
+  },
+  mounted () {
+    this.caseStudies = this.clientData.caseStudies
+  }
+}
+</script>
